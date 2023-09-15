4f95ecc05592fe150f64c8e7f7ba421a5f42b4b1
krishnasai <krishnasai2898@gmail.com>
2023-09-15 11:59:44 +0530

    randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+//to get reid of css scoping below is the step
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co", //when ever we run production it will generate class names by using ma instead of jss
+});
 
 export default () => {
   return (
     <BrowserRouter>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider , createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+//to get reid of css scoping below is the step
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',   //when ever we run production it will generate class names by using ma instead of jss
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
